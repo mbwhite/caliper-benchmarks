426ccf0f16cb14f508f46768413f07c7a8b3b9c2
Matthew B. White <whitemat@uk.ibm.com>
2019-10-18 16:14:32 +0100

    Add in a Java plus Caliper configuration
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

--- go.sh
+++ go.sh
@@ -1,6 +1,7 @@
  
 # Java do nothing contract 
-caliper benchmark run --caliper-benchconfig benchmarks/api/fabric/couchDB/empty-contract-2of.yaml  --caliper-networkconfig networks/fabric/fabric-v1.4.1/2org1peercouchdb/api/fabric-api-java.yaml --caliper-workspace . --caliper-fabric-usegateway
+caliper benchmark run --caliper-benchconfig benchmarks/api/fabric/couchDB/empty-contract-2of.yaml  --caliper-networkconfig networks/fabric/fabric-v1.4.1/2org1peercouchdb/api/fabric-api-java-prometheus.yaml --caliper-workspace . --caliper-fabric-usegateway
+
 #node /home/matthew/github.com/caliper/packages/caliper-cli/caliper.js benchmark run --caliper-benchconfig benchmarks/api/fabric/couchDB/get-asset.yaml  --caliper-networkconfig networks/fabric/fabric-v1.4/2org1peercouchdb/fabric-api-java.yaml --caliper-workspace . --caliper-fabric-usegateway
 
 # Node do nothing contract 
